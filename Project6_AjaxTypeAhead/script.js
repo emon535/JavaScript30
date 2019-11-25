b0f48683f818996aea04fa621b07a70f31b7cee5
emon535@gmail.com <emon535@gmail.com>
2019-11-25 18:32:12 +0600

    Project 6 completed

--- Project6_AjaxTypeAhead/script.js
+++ Project6_AjaxTypeAhead/script.js
@@ -2,3 +2,52 @@
     console.log("Hello from Console")
 })();
 
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+const promise = fetch(endpoint);
+console.log(promise);
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+
+// console.log(cities)
+
+function findMatches(searchWord, cities) {
+    let regexPattern = RegExp(searchWord, 'gi');
+
+    return cities.filter(place => {
+        return place.city.match(regexPattern) || place.state.match(regexPattern);
+
+    });
+}
+
+
+
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+
+searchInput.addEventListener('keyup', displayMatches);
+searchInput.addEventListener('change', displayMatches);
+
+
+function displayMatches() {
+    console.log(findMatches(this.value, cities));
+    const matchedCityArray = findMatches(this.value, cities);
+    console.log(matchedCityArray);
+
+    const html = matchedCityArray.map(places => {
+        return `
+            <li>
+                <span class="name">${places.city}, ${places.state} </span>
+                <span>${places.population}</span>
+            </li>
+        `;
+    }).join("");
+    suggestions.innerHTML = html;
+
+}
